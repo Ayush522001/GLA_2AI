c5ac88f83c44c97d33a6f85e31e9fcb7e5f1483f
Ayush-CB <130039413+Ayush-CB@users.noreply.github.com>
2023-08-08 14:41:52 +0530

    Basics

---
+++ Lec_1/Operators.java
@@ -0,0 +1,17 @@
+package Lec_1;
+
+public class Operators {
+    public static void main(String[] args) {
+        int divide=3/8;
+        divide+=4;
+//        divide+=1;
+        int mod=3%8;
+        System.out.println(divide);
+        System.out.println(mod);
+
+        System.out.println(3>=3 && 4==4 && 3>3 && 7==7);
+        System.out.println(3>=3 || 4==4 || 3>3 || 7==7);
+
+
+    }
+}
---
+++ Lec_1/Variables.java
@@ -0,0 +1,18 @@
+package Lec_1;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+        System.out.print(a);
+        System.out.println("Hello world");
+
+//        int 2d=20;
+        int u2e=22;
+//        int e@=12;
+//        int y u=77;
+        int _ad=23;
+
+    }
+}
